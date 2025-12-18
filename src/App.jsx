fcfe776e1fb696e40b631342d4b61b5254e386f5
Ishanxpun <punishan500@gmail.com>
2025-12-18 16:38:06 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ThemeProvider from './providers/ThemeProvider'
-import Content from './components/content'
+import Content from './components/Content'
 
 const App = () => {
   return (
